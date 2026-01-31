fda17854fcf0592804df5937c754bf7d460070c9
baohuynh1 <bao15062002@gmail.com>
2026-01-31 13:05:30 +0700

    Day1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import { BrowserRouter, Route, Router,Routes } from 'react-router-dom';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './Components/Header';
+import Home from './Pages/Home';
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+    <Route path ="/" exact={true} element={<Home/>} /></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/index.js
@@ -0,0 +1,20 @@
+
+
+
+const Header = () => {
+  return (
+    <>
+    <div className="header">
+        <div className="top-strip bg-blue">
+            <div className="container">
+                <p className="mb-0 mt-0 text-center">Hello World<b>2026</b></p>
+            </div>
+
+        </div>
+
+    </div>
+    </>
+  );
+};
+
+export default Header;
