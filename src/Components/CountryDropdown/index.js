17af7ce31fbf984a833806c49589c58fc5ff3b97
baohuynh1 <bao15062002@gmail.com>
2026-02-02 13:02:31 +0700

    Day3

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,15 +1,55 @@
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 import { FaAngleDown } from "react-icons/fa";
+import Dialog from '@mui/material/Dialog';
+import { IoSearch } from "react-icons/io5";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import Slide from '@mui/material/Slide';
+import React from 'react';
+const Transition = React.forwardRef(function Transition(props, ref) {
+  
+  
+  return <Slide direction="up" ref={ref} {...props} />;
+})
+
 const CountryDropdown=() => {
-    return (
+    const [isOpenModal, setIsOpenModal]=useState(false);
+    
+  return (
     <>
-    <Button className='countryDrop'>
+    <Button className='countryDrop' onClick={()=>setIsOpenModal(true)}>
                   <div className='info d-flex flex-column'>
                     <span className='lable' >Your Location</span>
                     <span className='name'>VietNam</span>
                   </div>
                     <span className='ml-auto'  ><FaAngleDown></FaAngleDown></span>
                   </Button>
+
+                  <Dialog  open={isOpenModal} onClose={()=>setIsOpenModal(false)} className='locationModal'slots={{transition: Transition,}}>
+                    <h4 className='mb-0'>Choose your Delivery location</h4>
+                    <p>Enter your address and we will specify the offer for your arena</p>
+                    <Button className='close_' onClick={()=>setIsOpenModal(false)}><IoIosCloseCircleOutline />
+
+                    </Button>
+                    <div className='headerSearch w-100'>
+                  <input type="text"  placeholder='Search your arena'/>
+                  <Button><IoSearch></IoSearch></Button>
+                    </div>
+                    <ul className='countryList mt-3'>
+                      <li><Button onClick={()=>setIsOpenModal(false)}>VietNam</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}>Japan</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}>Bangladesh</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}>Bangladesh</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}> Bangladesh</Button></li>
+                        <li><Button onClick={()=>setIsOpenModal(false)}>VietNam</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}>Japan</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}>Bangladesh</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}>Bangladesh</Button></li>
+                      <li><Button onClick={()=>setIsOpenModal(false)}> Bangladesh</Button></li>
+
+                    </ul>
+      
+    </Dialog>
     </>
     )
 }
